7839052b73d9903b4436f70060d7a857452b5215
Group 10] <aqw5781@psu.edu>
2025-09-11 10:18:05 -0400

    Edited python file for Lab1, errors present

--- lab-devel/packages/lab1/src/mmdriver.py
+++ lab-devel/packages/lab1/src/mmdriver.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
+
+#drive one meter straight ninety repeat make square 
+
+
 import rospy
 import os
 from duckietown_msgs.msg import WheelsCmdStamped # Import the message for the wheel comm
 class Driver():#CHANGE CLASSNAME to the name of your class
     def __init__(self):
         self.veh_name = os.environ['VEHICLE_NAME']
+        self.cmdDrive = WheelsCmdStamped()
+        self.cmdDrive.vel_left = 1
+        self.cmdDrive.vel_right= 1
+        self.cmdTurn = WheelsCmdStamped()
+        self.cmdTurn.vel_left = 1
+        self.cmdTurn.vel_right= -1
 # USING PARAMETER TO GET THE NAME OF THE VEHICLE
 # THIS WILL BE USEFUL TO SPECIFY THE NAME OF THE TOPIC
 # INITIALIZE YOUR VARIABLES HERE (SUBSCRIBERS OR PUBLISHERS)
 
     def drive(self): # CHANGE TO THE NAME OF YOUR FUNCTION
+        #input commands here i guess 
+        #/ee483mm/wheels_driver_node is the topic being published to 
+        cmdDrive()
         print("Driving the MM " + self.veh_name + " around the block") # Just for testin
 #WRITE THE CODE TO MAKE THE MM GO AROUND THE BLOCK
 if __name__ == "__main__": ## The main function which will be called when your python sc
@@ -19,8 +32,9 @@ if __name__ == "__main__": ## The main function which will be called when your p
         drive = Driver() # Create obj of the Driver class
         rospy.sleep(3) # Delay to wait enough time for the code to run
         # Keep the line above - you might be able to reduce the delay a bit,
-        while not rospy.is_shutdown(): # Run ros forever - you can change
+        start = rospy.get_rostime()
+        while not rospy.is_shutdown() and float(rospy.get_rostime() - start) < 2: # Run ros forever - you can change
             # this as well instead of running forever
-         drive.drive() # calling your node function
+            drive.drive() # calling your node function
     except rospy.ROSInterruptException:
         pass
