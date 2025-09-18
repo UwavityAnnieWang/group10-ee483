aca30e1ed953eeacffdbe645097d7631ceeb9013
Group 10] <aqw5781@psu.edu>
2025-09-18 10:27:26 -0400

    Edited mmdriver.py, moves but needs work on timing

--- lab-devel/packages/lab1/src/mmdriver.py
+++ lab-devel/packages/lab1/src/mmdriver.py
@@ -10,8 +10,7 @@ class Driver():#CHANGE CLASSNAME to the name of your class
     def __init__(self):
         self.veh_name = os.environ['VEHICLE_NAME']
 
-        self.drivePub = rospy.Publisher('/ee483mm10/wheels_driver_node', WheelsCmdStamped, queue_size=10)
-        self.turnPub = rospy.Publisher('/ee483mm10/wheels_driver_node', WheelsCmdStamped, queue_size=10)
+        self.drivePub = rospy.Publisher('/ee483mm15/wheels_driver_node/wheels_cmd', WheelsCmdStamped, queue_size=10)
 
 # USING PARAMETER TO GET THE NAME OF THE VEHICLE
 # THIS WILL BE USEFUL TO SPECIFY THE NAME OF THE TOPIC
@@ -22,18 +21,27 @@ class Driver():#CHANGE CLASSNAME to the name of your class
         #/ee483mm/wheels_driver_node is the topic being published to 
         self.cmdDrive = WheelsCmdStamped()
         self.cmdDrive.header.stamp = rospy.Time.now()
-        self.cmdDrive.vel_left = 1
-        self.cmdDrive.vel_right= 1
+        self.cmdDrive.vel_left = 0.4
+        self.cmdDrive.vel_right= 0.4
         self.drivePub.publish(self.cmdDrive)
         print("Driving the MM " + self.veh_name + " around the block") # Just for testin
 
     def turn(self):
         self.cmdTurn = WheelsCmdStamped()
         self.cmdTurn.header.stamp = rospy.Time.now()
-        self.cmdTurn.vel_left = 1
-        self.cmdTurn.vel_right = 1
-        self.turnPub.publish(self.cmdTurn)
+        self.cmdTurn.vel_left = 0.3
+        self.cmdTurn.vel_right = -0.3
+        self.drivePub.publish(self.cmdTurn)
         print("turning")
+
+    def stop(self):
+        self.cmdSleep = WheelsCmdStamped()
+        self.cmdSleep.header.stamp = rospy.Time.now()
+        self.cmdSleep.vel_left = 0
+        self.cmdSleep.vel_right = 0
+        self.drivePub.publish(self.cmdSleep)
+        print("stopped")
+
 
 #WRITE THE CODE TO MAKE THE MM GO AROUND THE BLOCK
 if __name__ == "__main__": ## The main function which will be called when your python sc
@@ -45,12 +53,23 @@ if __name__ == "__main__": ## The main function which will be called when your p
         # Keep the line above - you might be able to reduce the delay a bit,
         cycles_max = 4
         cycle = 0
+        startTime = rospy.Time.now().to_sec()
+        print(startTime)
         while not rospy.is_shutdown() and cycle < cycles_max: # Run ros forever - you can change
             # this as well instead of running forever
             drive.drive()
-            rospy.sleep(1)
+            while rospy.Time.now().to_sec() < float(startTime + 2 + cycle*13):
+                pass
+            drive.stop()
+            while rospy.Time.now().to_sec() < float(startTime + 7 + cycle*13):
+                pass
             drive.turn()
-            rospy.sleep(1)
+            moveTime = rospy.Time.now()
+            while rospy.Time.now().to_sec() < float(startTime + 8 + cycle*13):
+                pass
+            drive.stop()
+            while rospy.Time.now().to_sec() < float(startTime + 13 + cycle*13):
+                pass
             cycle += 1
     except rospy.ROSInterruptException:
         pass
